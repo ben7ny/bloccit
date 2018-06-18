d281fee526541ced0929637fb2d633b9c0f6b22a
ben7ny <benjason777@gmail.com>
2018-06-18 14:22:19 -0400

    FAQ page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get "welcome/index"
 
-   get "welcome/about"
+  get "welcome/about"
 
   get "welcome/contact"
 
+  get "welcome/faq"
 
    root 'welcome#index'
 end
