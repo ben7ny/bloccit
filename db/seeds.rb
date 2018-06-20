79d7de170e1732c15e22a6839c242647536dcde6
ben7ny <benjason777@gmail.com>
2018-06-20 15:08:36 -0400

    problem with ecuting rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'random_data'
+
+50.times do
+  Post.create!(
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "#{Post.count}"
+Post.find_or_create_by(title: "My new title", body:"My new body")
+puts "#{Post.count}"
+
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
