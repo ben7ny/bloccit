6c2d46a9fa7e3eb5e8d32aceca5025975a76d410
ben7ny <benjason777@gmail.com>
2018-07-20 01:42:00 -0400

    doesn't send email

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   after_create :create_vote
+  after_create :create_favorite
 
   default_scope { order('rank DESC') }
 
@@ -32,6 +33,11 @@ class Post < ApplicationRecord
      update_attribute(:rank, new_rank)
    end
 
+   def create_favorite
+     Favorite.create(post: self, user: self.user)
+     favoriteMailer.new_post(self).deliver_now
+   end
+    
    private
 
    def create_vote
