d579f98fe809ea98e8387e7f77d74010b0b4e64c
ben7ny <benjason777@gmail.com>
2018-07-23 23:28:14 -0400

    done with sponsored post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
-  
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
