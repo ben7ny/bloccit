e5b29cd8740b77d04eec28bb03eb3be27d4a974b
ben7ny <benjason777@gmail.com>
2018-07-24 16:49:24 -0400

    topic and post are revised

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,106 @@
 class TopicsController < ApplicationController
 
-  before_action :require_sign_in, except: [:index, :show]
-  # before_action :authorize_user, except: [:index, :show]
+#   before_action :require_sign_in, except: [:index, :show]
+#
+#   def index
+#     @topics = Topic.all
+#   end
+#
+#   def show
+#     @topic = Topic.find(params[:id])
+#   end
+#
+#   def new
+#     if current_user.admin?
+#       @topic = Topic.new
+#     else
+#       flash[:alert] = "You must be an admin to do that."
+#       redirect_to topics_path
+#     end
+#   end
+#
+#   def create
+#     if current_user.admin?
+#       @topic = Topic.new(topic_params)
+#
+#       if @topic.save
+#         redirect_to @topic, notice: "Topic was saved successfully."
+#       else
+#         flash.now[:alert] = "Error creating topic. Please try again."
+#         render :new
+#       end
+#     else
+#       flash[:alert] = "You must be an admin to do that."
+#       redirect_to topics_path
+#     end
+#   end
+#
+#   def edit
+#     if current_user.admin? || current_user.moderator?
+#       @topic = Topic.find(params[:id])
+#     else
+#       flash[:alert] = "You must be an admin to do that."
+#       redirect_to topics_path
+#     end
+#   end
+#
+#   def update
+#     if current_user.admin? || current_user.moderator?
+#       @topic = Topic.find(params[:id])
+#       @topic.assign_attributes(topic_params)
+#
+#       if @topic.save
+#         flash[:notice] = "Topic was updated."
+#         redirect_to @topic
+#       else
+#         flash.now[:alert] = "Error saving topic. Please try again."
+#         render :edit
+#       end
+#     else
+#       flash[:alert] = "You must be an admin to do that."
+#       redirect_to topics_path
+#     end
+#   end
+#
+#   def destroy
+#     if current_user.admin?
+#       @topic = Topic.find(params[:id])
+#
+#       if @topic.destroy
+#         flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+#         redirect_to action: :index
+#       else
+#         flash.now[:alert] = "There was an error deleting the topic."
+#         render :show
+#       end
+#     else
+#       flash[:alert] = "You must be an admin to do that."
+#       redirect_to topics_path
+#     end
+#   end
+#
+#   private
+#   def topic_params
+#     params.require(:topic).permit(:name, :description, :public)
+#   end
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+before_action :require_sign_in, except: [:index, :show]
+before_action :authorize_user, except: [:index, :show]
 
   def index
     @topics = Topic.all
@@ -11,84 +110,96 @@ class TopicsController < ApplicationController
     @topic = Topic.find(params[:id])
   end
 
+  # def new
+  #   @topic = Topic.new
+  # end
+  #
+  # def create
+  #   @topic = Topic.new(topic_params)
+  #
+  #   if @topic.save
+  #     redirect_to @topic, notice: "Topic was saved successfully."
+  #   else
+  #     flash.now[:alert] = "Error creating topic. Please try again."
+  #     render :new
+  #   end
+  # end
+
+
   def new
-    if current_user.admin?
-      @topic = Topic.new
-    else
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
-    end
-  end
-
-  def create
-    if current_user.admin?
-      @topic = Topic.new(topic_params)
-
-      if @topic.save
-        redirect_to @topic, notice: "Topic was saved successfully."
+      if current_user.admin?
+        @topic = Topic.new
       else
-        flash.now[:alert] = "Error creating topic. Please try again."
-        render :new
+        flash[:alert] = "You must be an admin to do that."
+        redirect_to topics_path
       end
-    else
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
     end
-  end
+
+    def create
+      if current_user.admin?
+        @topic = Topic.new(topic_params)
+
+        if @topic.save
+          redirect_to @topic, notice: "Topic was saved successfully."
+        else
+          flash.now[:alert] = "Error creating topic. Please try again."
+          render :new
+        end
+      else
+        flash[:alert] = "You must be an admin to do that."
+        redirect_to topics_path
+      end
+    end
+
+
+
+
+
+
+
+
+
 
   def edit
-    if current_user.admin? || current_user.moderator?
-      @topic = Topic.find(params[:id])
-    else
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
-    end
+    @topic = Topic.find(params[:id])
   end
 
   def update
-    if current_user.admin? || current_user.moderator?
-      @topic = Topic.find(params[:id])
-      @topic.assign_attributes(topic_params)
+     @topic = Topic.find(params[:id])
+     @topic.assign_attributes(topic_params)
 
-      if @topic.save
+     if @topic.save
         flash[:notice] = "Topic was updated."
-        redirect_to @topic
-      else
-        flash.now[:alert] = "Error saving topic. Please try again."
-        render :edit
-      end
-    else
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
-    end
-  end
+       redirect_to @topic
+     else
+       flash.now[:alert] = "Error saving topic. Please try again."
+       render :edit
+     end
+   end
 
-  def destroy
-    if current_user.admin?
-      @topic = Topic.find(params[:id])
+   def destroy
+     @topic = Topic.find(params[:id])
 
-      if @topic.destroy
-        flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
-        redirect_to action: :index
-      else
-        flash.now[:alert] = "There was an error deleting the topic."
-        render :show
-      end
-    else
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
-    end
-  end
+     if @topic.destroy
+       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+       redirect_to action: :index
+     else
+       flash.now[:alert] = "There was an error deleting the topic."
+       render :show
+     end
+   end
 
-  private
-  def topic_params
-    params.require(:topic).permit(:name, :description, :public)
-  end
+   private
+   def topic_params
+     params.require(:topic).permit(:name, :description, :public)
+   end
 
-  # def authorize_user
-  #   unless current_user.admin?
-  #     flash[:alert] = "You must be an admin to do that."
-  #     redirect_to topics_path
-  #   end
-  # end
+   def authorize_user
+     unless current_user.admin? || current_user.moderator?
+       flash[:alert] = "You must be an admin to do that."
+       redirect_to topics_path
+     end
+   end
+
+
 end
